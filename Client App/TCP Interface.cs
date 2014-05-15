c5372dff3bb006f5112a54d84ada98eaed35e5a4
DeLiu <de.liu@ofir.dk>
2014-05-15 14:10:13 +0200

    Added the basic TCP in server

--- Client App/TCP Interface.cs
+++ Client App/TCP Interface.cs
@@ -8,5 +8,6 @@ namespace Client_App
 {
     class TCP_Interface
     {
+
     }
 }
---
+++ Server App/TCP Service.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server_App
+{
+    class TCP_Service
+    {
+        private static TCP_Service _instance;
+
+        private static TCP_Service Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new TCP_Service();
+                }
+                return _instance;
+            }
+        }
+
+        private TCP_Service()
+        {
+            IPAddress ipAd = IPAddress.Parse("127.0.0.1");
+            int portnumb = 65535;
+            TcpListener clientList = new TcpListener(ipAd, portnumb);
+
+            clientList.Start();
+
+            Socket s = clientList.AcceptSocket();
+            if (s.Connected)
+            {
+                NetworkStream stream = new NetworkStream(s);
+            }
+
+        }
+    }
+}
