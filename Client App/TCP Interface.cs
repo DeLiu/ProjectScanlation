2b70c029ea85eb1e477899ad0c96b15a82edc122
DeLiu <de.liu@ofir.dk>
2014-05-22 12:49:07 +0200

    Update TCP service/interface

--- Client App/TCP Interface.cs
+++ Client App/TCP Interface.cs
@@ -3,11 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
 
 namespace Client_App
 {
     class TCP_Interface
     {
+        private NetworkStream stream;
+        private TcpClient client;
+        private void Connect()
+        {
+            client = new TcpClient();
+            client.Connect("127.0.0.1", 65535);
+            stream = client.GetStream();
+            BinaryReader reader = new BinaryReader(stream);
+            BinaryWriter writer = new BinaryWriter(stream);
+        }
+  
+        private void Download()
+        {
+            byte[] streambuffer = new byte[buffersize];
+            
+        }
 
+        private void Upload()
+        {
+            byte[] streambuffer = File.ReadAllBytes(filepath);
+            try
+            {
+                stream.Write(streambuffer, 0, streambuffer.Length);
+            }
+            catch (SocketException e1)
+            {
+                //MessageBox.Show("Socketexception: " + e1); 
+            }
+            client.Close();
+        }
+
+        private void Release()
+        {
+
+        }
     }
 }
--- Server App/TCP Service.cs
+++ Server App/TCP Service.cs
@@ -5,12 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
+using System.IO;
 
 namespace Server_App
 {
     class TCP_Service
     {
         private static TCP_Service _instance;
+        private NetworkStream stream;
 
         private static TCP_Service Instance
         {
@@ -35,9 +37,43 @@ namespace Server_App
             Socket s = clientList.AcceptSocket();
             if (s.Connected)
             {
-                NetworkStream stream = new NetworkStream(s);
+                stream = new NetworkStream(s);
+                BinaryReader reader = new BinaryReader(stream);
+                BinaryWriter writer = new BinaryWriter(stream);
+
+                try
+                {
+                    while (true)
+                    {
+                        switch (reader.ReadString())
+                        {
+                            case "DOWN": Console.WriteLine("Download initiated."); break;
+                            case "UPLD": Console.WriteLine("Upload initiated."); break;
+                        }
+
+
+
+                    }
+                }
+                catch (Exception exc) { } // client closes, do nothing, but stop communicating
+                Console.WriteLine("Client has left the building, so server TCP also stops now.");
             }
 
+        }
+
+        private void Download()
+        {
+
+        }
+
+        private void Upload()
+        {
+            byte[] bytebuffer = new byte[1024];
+
+            using (FileStream filetest = File.Create(@"c:\temp\imagetest.png"))
+            {
+                stream.CopyTo(filetest);
+            }
         }
     }
 }
